f24bcfb0c7fe8dc6c2344bca3378b3a2490e0e70
Xuan <127477394+xuanlin129@users.noreply.github.com>
2024-06-06 15:50:20 +0800

    Add files via upload

--- js/safari_vh.js
+++ js/safari_vh.js
@@ -1,8 +1,10 @@
 function safariHacks() {
   let windowsVH = window.innerHeight / 100;
-  document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
+  document.body.style.setProperty('--vh', windowsVH + 'px');
+  document.querySelector('.bg').style.setProperty('--vh', windowsVH + 'px');
   window.addEventListener('resize', function() {
-      document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
+      document.body.style.setProperty('--vh', windowsVH + 'px');
+      document.querySelector('.bg').style.setProperty('--vh', windowsVH + 'px');
   });
 }
 
